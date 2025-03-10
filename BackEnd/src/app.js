804b6df0f3734f7c1778f299ebd3f7a0657b5a1d
yashpaunikar2000 <yashpaunikar2000@gmail.com>
2025-03-11 01:00:32 +0530

    third commit

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -2,7 +2,11 @@ const express=require('express');
 const aiRoutes = require('./Routes/ai.routes')
 const cors=require('cors')
 const app=express()
-app.use(cors("*"))
+app.use(cors({
+    origin: 'https://code-reviewer-frontend-three.vercel.app', // Specific frontend origin
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization'
+  }));
 app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Hello World!')
